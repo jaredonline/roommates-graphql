542e38874b46ed839bb8e1c1634f0dc6f590070c
Jared McFarland <jared.online@gmail.com>
2015-11-19 18:07:00 +0100

    moving more things around

--- roommates-graphql.go
+++ roommates-graphql.go
@@ -3,63 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	// internal
-	"github.com/jaredonline/roommates-graphql/data"
-	"github.com/jaredonline/roommates-graphql/types"
-
-	// external
-	"github.com/graphql-go/graphql"
+	"github.com/jaredonline/roommates-graphql/schema"
 )
 
 func main() {
-	dbMap, err := data.InitDB("development")
-
-	fields := graphql.Fields{
-		"users": &graphql.Field{
-			Type: graphql.NewList(types.User),
-			Resolve: func(p graphql.ResolveParams) interface{} {
-				return data.GetAllUsers(dbMap)
-			},
-		},
-		"user": &graphql.Field{
-			Type: types.User,
-			Resolve: func(p graphql.ResolveParams) interface{} {
-				if idQuery, isOK := p.Args["id"].(int); isOK {
-					return data.GetUserById(dbMap, idQuery)
-				}
-				return nil
-			},
-			Args: graphql.FieldConfigArgument{
-				"id": &graphql.ArgumentConfig{
-					Type: graphql.Int,
-				},
-			},
-		},
-	}
-
-	rootQuery := graphql.ObjectConfig{
-		Name:   "RootQuery",
-		Fields: fields,
-	}
-
-	schemaConfig := graphql.SchemaConfig{
-		Query: graphql.NewObject(rootQuery),
-	}
-
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create schema: %v", err)
-	}
-
 	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.Query()["query"][0]
-		result := graphql.Do(graphql.Params{
-			Schema:        schema,
-			RequestString: query,
-		})
+		result := schema.ExecuteQuery(query)
 		json.NewEncoder(w).Encode(result)
 	})
 
---
+++ schema/queries.go
@@ -0,0 +1,69 @@
+package schema
+
+import (
+	"log"
+
+	// internal
+	"github.com/jaredonline/roommates-graphql/data"
+	"github.com/jaredonline/roommates-graphql/types"
+
+	// external
+	"github.com/graphql-go/graphql"
+	"gopkg.in/gorp.v1"
+)
+
+var (
+	dbMap  *gorp.DbMap
+	fields graphql.Fields
+	schema graphql.Schema
+	err    error
+)
+
+func init() {
+	dbMap, err = data.InitDB("development")
+	fields := graphql.Fields{
+		"users": &graphql.Field{
+			Type: graphql.NewList(types.User),
+			Resolve: func(p graphql.ResolveParams) interface{} {
+				return data.GetAllUsers(dbMap)
+			},
+		},
+		"user": &graphql.Field{
+			Type: types.User,
+			Resolve: func(p graphql.ResolveParams) interface{} {
+				if idQuery, isOK := p.Args["id"].(int); isOK {
+					return data.GetUserById(dbMap, idQuery)
+				}
+				return nil
+			},
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig{
+		Name:   "RootQuery",
+		Fields: fields,
+	}
+
+	schemaConfig := graphql.SchemaConfig{
+		Query: graphql.NewObject(rootQuery),
+	}
+
+	schema, err = graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create schema: %v", err)
+	}
+}
+
+func ExecuteQuery(query string) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+
+	return result
+}
