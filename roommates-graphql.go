7cd734fc680ee31be2321b0191c5f83f212750b1
Jared McFarland <jared.online@gmail.com>
2015-11-19 06:56:29 +0100

    graphql + postgres works

--- roommates-graphql.go
+++ roommates-graphql.go
@@ -1,15 +1,83 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 
 	// external
 	"github.com/graphql-go/graphql"
+	_ "github.com/lib/pq"
 )
 
+var userType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.FieldConfigMap{
+			"first_name": &graphql.FieldConfig{
+				Type: graphql.String,
+			},
+			"last_name": &graphql.FieldConfig{
+				Type: graphql.String,
+			},
+			"age": &graphql.FieldConfig{
+				Type: graphql.String,
+			},
+			"id": &graphql.FieldConfig{
+				Type: graphql.String,
+			},
+		},
+	},
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age       string `json:"age"`
+}
+
+func getUsers() []interface{} {
+	users := make([]interface{}, 0)
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var (
+			id        int
+			firstName string
+			lastName  string
+			age       int
+		)
+
+		rows.Scan(&id, &firstName, &lastName, &age)
+
+		users = append(users, User{
+			ID:        fmt.Sprintf("%d", id),
+			FirstName: firstName,
+			LastName:  lastName,
+			Age:       fmt.Sprintf("%d", age),
+		})
+	}
+
+	return users
+}
+
 func main() {
+	db, err = sql.Open("postgres", "user=jared dbname=mydb sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fields := graphql.FieldConfigMap{
 		"hello": &graphql.FieldConfig{
 			Type: graphql.String,
@@ -21,6 +89,18 @@ func main() {
 			Type: graphql.String,
 			Resolve: func(p graphql.GQLFRParams) interface{} {
 				return "bar"
+			},
+		},
+		"users": &graphql.FieldConfig{
+			Type: graphql.NewList(userType),
+			Resolve: func(p graphql.GQLFRParams) interface{} {
+				return getUsers()
+			},
+		},
+		"user": &graphql.FieldConfig{
+			Type: userType,
+			Resolve: func(p graphql.GQLFRParams) interface{} {
+				return getUsers()[0]
 			},
 		},
 	}
@@ -41,9 +121,12 @@ func main() {
 
 	// Query
 	query := `
-		{
-			hello
-			foo
+		query GetUsers {
+			users {
+				first_name
+				last_name
+				age
+			}
 		}
 	`
 
